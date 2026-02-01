1628ff0992d0c8f22d5c41a754c552a66542e5a8
unreal <nhunghoang2512@gmail.com>
2026-02-01 08:12:19 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,7 +7,7 @@ addLayer("p", {
 		points: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
